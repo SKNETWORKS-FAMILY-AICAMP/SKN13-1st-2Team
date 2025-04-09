3604529d3e7c928e81069454e9fb587c623ae1cf
WooZhoon <jhwoo0412@gmail.com>
2025-04-09 21:23:14 +0900

    up

--- crawling/test_crawling_from_consumer24.py
+++ crawling/test_crawling_from_consumer24.py
@@ -12,88 +12,177 @@ from selenium.webdriver.support import expected_conditions as EC
 
 # ✅ 브랜드 매핑 딕셔너리
 valid_brands = {
-    "GS글로벌", "기아", "닛산", "다임러트럭", "디암러트럭", "람보르기니", "롤스로이스", "루트17",
-    "르노", "르노삼성", "르노자동차", "르노코리아", "마세라티", "만트럭", "모빌리티네트웍스",
-    "벤츠", "벤츠코리아", "벤틀리", "볼보", "볼보코리아", "볼보트럭", "비엠더블유", "쌍용",
-    "쌍용자동차", "애스턴마틴", "이비온", "재규어랜드로버", "재규어랜드로보", "제이스모빌리티",
-    "지에스글로벌", "지엠아시아", "지엠코리아", "케이지모빌리티", "케이지엠커머셜", "테슬라",
-    "토요타", "포드", "포르쉐", "포르쉐코리아", "폭스바겐그룹", "한국닛산", "현대", "현대자동차",
-    "혼다", "혼다코리아"
-}
-
-brand_mapping = {
+    "GS글로벌", "기아", "기흥인터내셔널", 
+    #"닛산", 
+    "다임러트럭",
+    "디암러트럭", "람보르기니", "롤스로이스", "루트17", "르노", 
+    "르노삼성", "르노자동차", "르노코리아", "마세라티", # "만트럭",
+    "모빌리티네트웍스", "벤츠", "벤츠코리아", "벤틀리", "볼보",
+    "볼보코리아", "볼보트럭", "비엠더블유", #"스즈키씨엠씨", "스즈키코리아",
+    #"스텔란티스", 
+    "쌍용", "쌍용자동차", "애스턴마틴", "에프엠케이","에스에스라이트",
+    "이비온","자일대우", "재규어랜드로버", "재규어랜드로보", "제이스모빌리티", "지에스글로벌",
+    "지엠아시아", "지엠코리아", "케이지모빌리티", "케이지엠커머셜", "테슬라",
+    "토요타", "포드", "포르쉐", "포르쉐코리아", "폭스바겐그룹",
+    # "한국닛산", 
+    "한국지엠", "한불", "한불모터스", "현대",
+    "현대자동차", "혼다", "혼다코리아"
+    }
+brand_map = {
     "GS글로벌": "BYD",
-    "KR모터스": None,
     "기아": "기아",
-    "기흥": None,           # 수입사
-    "기흥모터스": None,     # 수입사
-    "기흥인터내셔널": None, # 수입사
-    "노바스": "대창모터스",
-    "닛산": None,
+    # "기흥인터내셔널": None,
+    # "닛산": "닛산",
     "다임러트럭": "벤츠",
-    "대전기계공업": None,
-    "더좋은사람": None,
-    "두카티": None,
+    "디암러트럭": "벤츠",
     "람보르기니": "람보르기니",
     "롤스로이스": "롤스로이스",
-    "루트17": None,
+    "루트17": "대창모터스",
     "르노": "르노",
+    "르노삼성": "르노",
     "르노자동차": "르노",
     "르노코리아": "르노",
     "마세라티": "마세라티",
-    "만트럭": None,
+    # "만트럭": "폭스바겐그룹",
     "모빌리티네트웍스": "모빌리티네트웍스",
-    "바이드": "BYD",
-    "바이크원": None,
-    "바이크코리아": None,
-    "범한자동차": "자일자동차",
     "벤츠": "벤츠",
+    "벤츠코리아": "벤츠",
     "벤틀리": "벤틀리",
     "볼보": "볼보",
+    "볼보코리아": "볼보",
     "볼보트럭": "볼보",
-    "블루샤크": None,
-    "비바모빌리티": None,
-    "비엠더블유": "BMW",
-    "스즈키씨엠씨": None,
-    "스카니아": None,
-    "스텔란티스": None,
-    "아이씨피": None,
+    # "스즈키씨엠씨": "스즈키",
+    # "스즈키코리아": "스즈키",
+    # "스텔란티스": None,
+    "쌍용": "KGM",
+    "쌍용자동차": "KGM",
     "애스턴마틴": "애스턴마틴",
-    "야마하": None,
     "에스에스라이트": "에스에스라이트",
-    "에프엠케이": "마세라티",  # 또는 페라리. 상황 따라 다르게 가능
-    "오토스원": None,
-    "이누리": None,
+    # "에프엠케이": None,
     "이비온": "이비온",
-    "인에이블인터내셔널": None,
-    "재규어랜드로버": "랜드로버",
-    "재규어랜드로보": "랜드로버",
+    "자일대우": "자일자동차",
+    "재규어랜드로버": "재규어랜드로버",
+    "재규어랜드로보": "재규어랜드로버",
     "제이스모빌리티": "제이스모빌리티",
-    "제이에스얼라이언스": None,
-    "지에스글로벌": None,
-    "지엠아시아": "쉐보레",
-    "지엠코리아": "쉐보레",
-    "케이지모빌리티": "쎄보모빌리티",
+    "지에스글로벌": "BYD",
+    # "지엠아시아": "GMC",
+    # "지엠코리아": "GMC",
+    "케이지모빌리티": "KGM",
     "케이지엠커머셜": "KGM",
-    "킹콩이브이코리아": None,
-    "타타대우": None,
     "테슬라": "테슬라",
-    "텐핑": None,
     "토요타": "토요타",
-    "티에스모터스": None,
     "포드": "포드",
     "포르쉐": "포르쉐",
-    "폭스바겐그룹": "폭스바겐",
-    "피라인모터스": None,
-    "한국닛산": None,
-    "한국모터스": None,
-    "한국모터트레이딩": None,
-    "한국지엠": "쉐보레",
-    "한솜바이크": None,
-    "현대": "현대",
+    "포르쉐코리아": "포르쉐",
+    # "폭스바겐그룹": "폭스바겐그룹", 
+    # "한국지엠": "쉐보레",
+    # "한국닛산": "닛산",
+    # "한불": None,
+    # "한불모터스": None,
     "혼다": "혼다",
-    "화창상사": None,
+    "혼다코리아": "혼다"
 }
+cadillac_models = ["CT4", "CT5", "CT6", "XT4", "XT5", "XT6", "ATS", "CTS", "STS", "에스컬레이드", "리릭"]
+lexus_models = [
+    "ES300h",
+    "RX450h",
+    "RZ450e",
+    "UX300h 2WD",
+    "UX250h 2WD",
+    "UX250h AWD",
+    "NX350h",
+    "NX450h+",
+    "RX350h",
+    "RX450h+",
+    "RX500h",
+    "LS500h AWD",
+    "LS500 AWD",
+    "LS500h 2WD",
+    "LS500h",
+    "CT200h",
+    "SC430"
+]
+audi_models = [
+        "A3", "A7", "A8", "Q3", "Q4", "Q6", "Q7", "Q8",
+        "RS", "S8", "SQ", "e-tron", "R8"
+        ]
+volkswagen_models = [
+        "Golf", "ID.4", "ID.5", "Jetta", "The Beetle", "Touareg", "Tiguan"
+        ]
+ford_models = [
+            "Mustang", "Bronco", "Explorer", "Exp", "Ranger"
+        ]
+lincorln_models = [
+            "Nautilus", "Aviator", "Corsair", "Navigator", "MKC", "MKX"
+        ]
+def map_brand(brand: str, name) -> str | None:
+    # 여러 항목을 다루는 brand 처리
+    ## 기흥인터내셔널널
+    if brand == "기흥인터내셔널":
+        if "McL" in name or "Art" in name or "Spi" in name:
+            return "맥라렌"
+    ## 스텔란티스 or 한불, 한불모터스스
+    elif brand == "스텔란티스" or "한불" in brand:
+        if "짚" in name:
+            return "지프"
+        elif "Peu" in name:
+            return "푸조"
+        elif "DS" in name:
+            return "DS"
+        elif "Cit" in name:
+            return "시트로엥"
+        else:
+            return None
+
+    ## 에프엠케이
+    elif brand == "에프엠케이":
+        if "페라리" in name:
+            return "페라리"
+        else:
+            return "마세라티"
+    # ## 한불, 한불 모터스
+    # elif "한불" in brand:
+    #     if "DS" in name:
+    #         return "DS"
+    #     elif "Peu" in name:
+    #         return "푸조"
+    #     else:
+    #         return "시트로엥"
+    ## 비엠더블유
+    elif brand == "비엠더블유":
+        if "MINI" in name:
+            return "미니"
+        else:
+            return "BMW"
+    elif "현대" in brand:
+        if name[0] =='G':
+            return "제네시스"
+        else:
+            return "현대"
+    elif "지엠" in brand :
+        if name in cadillac_models:
+            return "캐딜락"
+        elif '시에라' in name:
+            return "GMC"
+        else:
+            return "쉐보레"
+    elif brand=="토요타" and name in lexus_models:
+        return "렉서스"
+    elif brand == "폭스바겐그룹":
+        if any(keyword in name for keyword in audi_models):
+            return "아우디"
+        elif any(keyword in name for keyword in volkswagen_models):
+            return "폭스바겐"
+        return None
+    elif brand=='포드':
+        if any(keyword in name for keyword in ford_models):
+            return "포드"
+        
+        elif any(keyword in name for keyword in lincorln_models):
+            return "링컨"
+
+    
+    return brand_map.get(brand) if brand in valid_brands else None
 
 
 driver = webdriver.Chrome()
@@ -111,8 +200,6 @@ cursor = conn.cursor()
 
 url_list = [f'https://www.consumer.go.kr/user/ftc/consumer/recallInfo/629/selectRecallInfoInternalList.do?searchCondition1=0301&page={i}' for i in range(1, 95)]
 
-# def normalize_brand(raw_brand):
-#     return brand_mapping.get(raw_brand, None)
 
 for url in url_list:
     driver.get(url)
@@ -176,11 +263,11 @@ for url in url_list:
             model_list = [m.strip() for m in (model_name or '').split(',') if m.strip()]
             release_start = production_period.split('~')[0].strip() if production_period else None
             release_end = production_period.split('~')[1].strip() if production_period and '~' in production_period else None
-
-            # ✅ 브랜드명 매핑 적용
-            #  brand = brand_mapping.get(brand, brand)
-
+            temp_brand = brand
             for name in model_list or [None]:
+                # ✅ 브랜드명 매핑 적용
+                brand = map_brand(temp_brand,name)
+                print(brand, name)
                 cursor.execute("""
                     INSERT INTO recalls (brand, name, release_start, release_end, recall_type, announcement_start, announcement_end, source, defect_description)
                     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
