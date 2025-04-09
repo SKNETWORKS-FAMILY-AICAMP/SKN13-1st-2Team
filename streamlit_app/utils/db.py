4d88d4dca5e43b5e2e1328ec9be57ee5e5c05017
jaehoi koo <playdatakoo@gmail.com>
2025-04-09 14:40:03 +0900

    test.db 연동정보 수정

--- streamlit_app/utils/db.py
+++ streamlit_app/utils/db.py
@@ -1,11 +1,12 @@
 import pymysql
 
+# WOOZ Local DB Connection
 def get_connection():
     return pymysql.connect(
-        host="localhost",
-        user="root",
+        host="192.168.0.41",
+        user="skn13_woo",
         password="1111",
-        db="project",
+        db="car_data",
         charset="utf8mb4",
         cursorclass=pymysql.cursors.DictCursor
     )
